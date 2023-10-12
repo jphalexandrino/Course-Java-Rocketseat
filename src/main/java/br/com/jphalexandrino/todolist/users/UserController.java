e88dd818b9d440ec83a8475db3f9c5e1df1d3e56
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-12 23:40:55 +0000

    Adiciona a restrição de usuários com nomes iguais.

--- src/main/java/br/com/jphalexandrino/todolist/users/UserController.java
+++ src/main/java/br/com/jphalexandrino/todolist/users/UserController.java
@@ -1,6 +1,8 @@
 package br.com.jphalexandrino.todolist.users;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,9 +22,18 @@ public class UserController {
         private IUserRepository userRepository;
 
     @PostMapping("/")
-    public UserModel create(@RequestBody UserModel userModel){
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+
+        if(user != null){
+            System.out.println("Usuário já existe.");
+            //Mensagem de erro
+            //Status Code
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+
         var userCreated = this.userRepository.save(userModel);{
-            return userCreated;
+            return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
         }
     }
     
