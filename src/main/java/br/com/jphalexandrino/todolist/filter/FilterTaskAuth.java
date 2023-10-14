798cd047e3d089e238a8cd9187a8896c16ffa8de
João Pedro Alexandrino <131566983+sondercs@users.noreply.github.com>
2023-10-14 01:57:01 +0000

    Configura o filtro. Pega a senha do usuário e mostra ela no terminal.

--- src/main/java/br/com/jphalexandrino/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/jphalexandrino/todolist/filter/FilterTaskAuth.java
@@ -1,26 +1,55 @@
 package br.com.jphalexandrino.todolist.filter;
 
 import java.io.IOException;
+import java.util.Base64;
 
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
-import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 @Component
-public class FilterTaskAuth implements Filter {
+public class FilterTaskAuth extends OncePerRequestFilter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
-            throws IOException, ServletException {
-        //Executar alguma ação
-        
-        System.out.println("Chegou no filtro");
-        chain.doFilter(request, response);
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
 
+                // Pegar a autentificação
+                var authorization = request.getHeader("Authorization");
+
+
+                var authEncoded = authorization.substring("Basic".length()).trim();
+
+                byte[] authDecoded = Base64.getDecoder().decode(authEncoded);
+
+                var authString = new String(authDecoded);
+
+                System.out.println("Authorization");
+                System.out.println(authString);
+
+
+
+               // ["Jphalexandrino, 12345"]
+                String[] credentials = authString.split(":");
+                String username = credentials[0];
+                String password = credentials[1];
+                System.out.println("Authorization");
+                System.out.println(username);                
+                System.out.println(password);
+
+
+                // Validar o usuário
+
+                // Segue viagem
+                
+                filterChain.doFilter(request, response);    
     }
+
+
     
 }
